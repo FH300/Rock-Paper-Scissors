e1928a9e98a44928b042bb68f45c75c337399d1f
FH300 <xhbl1220@gmail.com>
2023-12-16 12:54:40 +0100

    index.html

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -51,75 +51,144 @@ let rockBtn = document.querySelector('#rock');
 let paperBtn = document.querySelector('#paper');
 let scissorsBtn = document.querySelector('#scissors');
 let resultsDiv = document.querySelector('#results');
+let resultsDivTwo = document.querySelector('#resultsTwo');
+let refereeDiv = document.querySelector('#referee');
+let resultsDivFull = document.querySelector('.resultsdivfull');
+let fullDiv = document.querySelector('.fulldiv');
 
 let yourScore = 0;
 let computerScore = 0;
 
+addEventListener('load', () => {
+  resultsDiv.textContent = 'Hit one button to start!'
+})
+
 rockBtn.addEventListener('click', () => {
   singleRound('ROCK', computerSelection);
-  console.log(referee);
   if (referee == 'you win') {
     yourScore++;
   } else if (referee == 'computer wins') {
     computerScore++;
   }
-  resultsDiv.textContent = `Your score: ${yourScore} ` + `Computer\'s score: ${computerScore}`;
+  resultsDiv.textContent = `Your score: ${yourScore} `;
+  resultsDivTwo.textContent = `Computer\'s score: ${computerScore}`;
+  refereeDiv.textContent = `${referee}`;
   if (yourScore >= 5) {
     resultsDiv.textContent = 'Your Score is 5, you won!!!';
+    resultsDivTwo.textContent = '';
+    refereeDiv.textContent = '';
     rockBtn.disabled = true;
     paperBtn.disabled = true;
     scissorsBtn.disabled = true;
+    let reloadButton = document.createElement('button');
+    reloadButton.setAttribute('id', 'reloadButton');
+    reloadButton.textContent = 'Play again';
+    reloadButton.addEventListener('click', () => {
+      location.reload();
+    });
+    fullDiv.appendChild(reloadButton);
   } else if (computerScore >= 5) {
     resultsDiv.textContent = 'You lost! The computer has won 5 times.';
+    resultsDivTwo.textContent = '';
+    refereeDiv.textContent = '';
     rockBtn.disabled = true;
     paperBtn.disabled = true;
     scissorsBtn.disabled = true;
+    let reloadButton = document.createElement('button');
+    reloadButton.setAttribute('id', 'reloadButton');
+    reloadButton.textContent = 'Try again';
+    reloadButton.addEventListener('click', () => {
+      location.reload();
+    });
+    fullDiv.appendChild(reloadButton);
   }
 });
 
 paperBtn.addEventListener('click', () => {
   singleRound('PAPER', computerSelection);
-  console.log(referee);
   if (referee == 'you win') {
     yourScore++;
   } else if (referee == 'computer wins') {
     computerScore++;
   }
-  resultsDiv.textContent = `Your score: ${yourScore} ` + `Computer\'s score: ${computerScore}`;
+  resultsDiv.textContent = `Your score: ${yourScore} `;
+  resultsDivTwo.textContent = `Computer\'s score: ${computerScore}`;
+  refereeDiv.textContent = `${referee}`;
   if (yourScore >= 5) {
     resultsDiv.textContent = 'Your Score is 5, you won!!!';
+    resultsDivTwo.textContent = '';
+    refereeDiv.textContent = '';
     rockBtn.disabled = true;
     paperBtn.disabled = true;
     scissorsBtn.disabled = true;
+    let reloadButton = document.createElement('button');
+    reloadButton.setAttribute('id', 'reloadButton');
+    reloadButton.textContent = 'Play again';
+    reloadButton.addEventListener('click', () => {
+      location.reload();
+    });
+    fullDiv.appendChild(reloadButton);
   } else if (computerScore >= 5) {
     resultsDiv.textContent = 'You lost! The computer has won 5 times.';
+    resultsDivTwo.textContent = '';
+    refereeDiv.textContent = '';
     rockBtn.disabled = true;
     paperBtn.disabled = true;
     scissorsBtn.disabled = true;
+    let reloadButton = document.createElement('button');
+    reloadButton.setAttribute('id', 'reloadButton');
+    reloadButton.textContent = 'Try again';
+    reloadButton.addEventListener('click', () => {
+      location.reload();
+    });
+    fullDiv.appendChild(reloadButton);
   }
 });
 
 scissorsBtn.addEventListener('click', () => {
   singleRound('SCISSORS', computerSelection);
-  console.log(referee);
   if (referee == 'you win') {
     yourScore++;
   } else if (referee == 'computer wins') {
     computerScore++;
   }
-  resultsDiv.textContent = `Your score: ${yourScore} ` + `Computer\'s score: ${computerScore}`;
+  resultsDiv.textContent = `Your score: ${yourScore} `;
+  resultsDivTwo.textContent = `Computer\'s score: ${computerScore}`;
+  refereeDiv.textContent = `${referee}`;
   if (yourScore >= 5) {
     resultsDiv.textContent = 'Your Score is 5, you won!!!';
+    resultsDivTwo.textContent = '';
+    refereeDiv.textContent = '';
     rockBtn.disabled = true;
     paperBtn.disabled = true;
     scissorsBtn.disabled = true;
+    let reloadButton = document.createElement('button');
+    reloadButton.setAttribute('id', 'reloadButton');
+    reloadButton.textContent = 'Play again';
+    reloadButton.addEventListener('click', () => {
+      location.reload();
+    });
+    fullDiv.appendChild(reloadButton);
   } else if (computerScore >= 5) {
     resultsDiv.textContent = 'You lost! The computer has won 5 times.';
+    resultsDivTwo.textContent = '';
+    refereeDiv.textContent = '';
     rockBtn.disabled = true;
     paperBtn.disabled = true;
     scissorsBtn.disabled = true;
+    let reloadButton = document.createElement('button');
+    reloadButton.setAttribute('id', 'reloadButton');
+    reloadButton.textContent = 'Try again';
+    reloadButton.addEventListener('click', () => {
+      location.reload();
+    });
+    fullDiv.appendChild(reloadButton);
   }
 });
+
+
+
+
 
 
 
